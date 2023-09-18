9bbcdcc5750e341a43c6bc71316565bfe9cd8eea
Peter Osoo <posoo617@gmail.com>
2023-09-18 14:33:18 +0300

    add & edit expense

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, add_expense, edit_expense
 
 urlpatterns = [
-    path('', index, name="home"),
+    path('', index, name="expense_list"),
+    path('add/', add_expense, name='add_expense'),
+    path('edit/<int:expense_id>/', edit_expense, name='edit_expense'),
+    #
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-from .models import Expense
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Expense
+from .forms import ExpenseForm
 
 
 def index(request):
     expenses = Expense.objects.all()
     return render(request, 'home.html', {'expenses': expenses})
+
+
+@login_required
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'tracker/expense_form.html', {'form': form})
+
+
+def edit_expense(request, expense_id):
+    expense = get_object_or_404(Expense, id=expense_id)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm(instance=expense)
+    return render(request, 'tracker/expense_form.html', {'form': form})
