b56b0f87f46740af6158dfa7fb3bd63dcbfcf6e0
Peter Osoo <posoo617@gmail.com>
2023-09-18 13:58:07 +0300

    display expenses on home page

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Expense
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'home.html')
+    expenses = Expense.objects.all()
+    return render(request, 'home.html', {'expenses': expenses})
