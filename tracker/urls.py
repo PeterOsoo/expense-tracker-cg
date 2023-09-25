08923669bf62a5e56123a38d69615fd4d4b8a9b1
Peter Osoo <posoo617@gmail.com>
2023-09-25 20:29:35 +0300

    home URL

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, add_expense, edit_expense, ExpenseDeleteView, register, login, logout_view
+from .views import (index, expense_list, add_expense, edit_expense,
+                    ExpenseDeleteView, register, login, logout_view)
 
 urlpatterns = [
-    path('', index, name="expense_list"),
+    path('', index, name="home"),
+    path('expense/', expense_list, name="expense_list"),
     path('add/', add_expense, name='add_expense'),
     path('edit/<int:expense_id>/', edit_expense, name='edit_expense'),
     path('expenses/delete/<int:pk>/', ExpenseDeleteView.as_view(),
