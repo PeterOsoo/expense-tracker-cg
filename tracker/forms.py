0ff90a712dbc4cff37158836f51a2840399b3cb1
Peter Osoo <posoo617@gmail.com>
2023-10-18 21:53:17 +0300

    CustomUserCreationForm

--- tracker/forms.py
+++ tracker/forms.py
@@ -2,6 +2,8 @@
 from django import forms
 from .models import Expense
 from datetime import date
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ExpenseForm(forms.ModelForm):
@@ -13,3 +15,14 @@ class ExpenseForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         self.fields['date'].initial = date.today()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name",
+                  "last_name", "password1", "password2")
