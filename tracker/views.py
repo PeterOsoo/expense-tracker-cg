ed6b1e14590918274ac0f6cb798e87911f729906
Peter Osoo <posoo617@gmail.com>
2023-11-09 13:08:33 +0300

    messages

--- tracker/views.py
+++ tracker/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth import login as auth_login, logout
 from django.shortcuts import render, redirect
 
 from .decorators import redirect_logged_in_user
+from django.contrib import messages
 
 
 from .models import Expense
@@ -91,12 +92,15 @@ def register(request):
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
             return redirect('expense_list')
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
 
+@redirect_logged_in_user
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
