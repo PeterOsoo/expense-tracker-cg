8bb8a4b1198bf45f819cf5f23ebe3ff1678b5088
Peter Osoo <posoo617@gmail.com>
2023-12-06 12:06:22 +0300

    remove blank=True

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(
-        upload_to='profile_pics/', blank=True, default='profile_pics/default_profile.png')
+        upload_to='profile_pics/', default='profile_pics/default_profile.png')
 
     def __str__(self):
         return f'{self.user.username} Profile'
