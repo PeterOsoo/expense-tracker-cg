d72cb00eba95e312b0dee633cccd8d866776efc1
Peter Osoo <posoo617@gmail.com>
2023-12-06 11:41:28 +0300

    media

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -139,4 +139,7 @@ STATICFILES_DIRS = [
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'expense_list'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
