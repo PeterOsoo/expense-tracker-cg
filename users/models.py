32fa96d00681373161fb7d73e704664cdd835369
Peter Osoo <posoo617@gmail.com>
2023-12-06 11:26:24 +0300

    change upload

--- users/models.py
+++ users/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/', blank=True, default='profile_pics/default_profile.png')
 
     def __str__(self):
         return self.user.username
