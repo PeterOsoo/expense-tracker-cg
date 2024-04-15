7d99ce5be1936d5ed0dc195e2226f734046f419b
Peter Osoo <posoo617@gmail.com>
2024-04-15 16:36:39 +0300

    test income list

--- income/urls.py
+++ income/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import income_list, add_income
+from .views import income_list, add_income, new_list
 
 urlpatterns = [
-    path('income/', income_list, name='income_list'),
-    path('income/add/', add_income, name='add_income'),
+    path('', income_list, name='income_list'),
+    path('add/', add_income, name='add_income'),
+    path('list/', new_list, name='new_list'),
 ]
